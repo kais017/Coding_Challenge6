99428d0c86a6414444f3f875e1e9ce66306c51d2
Your Name <your.email@example.com>
2025-02-09 15:57:29 -0500

    Task 6 - Higher-Order Function for Bulk Orders

--- cc_6.js
+++ cc_6.js
@@ -40,3 +40,25 @@ console.log(`Converted Amount: $${Math.floor(convertCurrency(100, 1.1))}`);
 console.log(`Converted Amount: $${convertCurrency(250, 0.85)}`);
 // logged the test data
 
+
+// Task 6: Higher Order Functions
+
+let orders = [200, 600, 1200, 450, 800];
+// creatng an array of ordes with order amounts
+
+function applyBulkDiscount(orders, discountFunction)
+// creating a function that calculates the discounted orders
+{ return orders.map(discountFunction);}
+// gives the output that applys the discount function to multiple orders
+
+let discount9 = (orders, amount => amount > 500 ? amount * 0.9: amount); 
+// if order numbers are above 500, the 10% discount is applied 
+
+let discountedOrders = applyBulkDiscount(orders,discount9); 
+// applies the discount to all orders above 500
+
+console.log("Bulk Discount:", discountedOrders);
+// logging the discounted orders
+
+
+
