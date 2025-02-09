cc0a77df14c9c51e5f3c7ee785e9186cc60525db
Your Name <your.email@example.com>
2025-02-09 11:19:13 -0500

    Task 1 - Business Profit Calculation

---
+++ cc_6.js
@@ -0,0 +1,15 @@
+
+// Task 1: Function Declaration
+
+function calculateProfit(costPrice, sellingPrice, unitsSold)
+// creating a function that calculates our total profit
+{ return ((sellingPrice - costPrice) * unitsSold) }
+// this will give us the calculation in our output
+
+console.log(`Total Profit: $${calculateProfit(20,30,100)}`)
+// testing data
+console.log(`Total Profit: $${calculateProfit(50,70,200)}`)
+// Logging the test data
+
+
+
